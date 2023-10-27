1bc8092a5cece991959497c73835b4787825970b
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-27 14:34:39 +0300

    layout

---
+++ layouts/LayoutTwo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fix">
+    <Header />
+    <main>
+      <slot></slot>
+    </main>
+    <footer-two/>
+    <BackToTop/>
+  </div>
+</template>
+
+<script>
+//import HeaderTwo from "./headers/HeaderTwo.vue";
+import Header from "./headers/HeaderNine.vue";
+import FooterTwo from "./footers/FooterTwo.vue";
+import BackToTop from "./footers/component/BackToTop.vue";
+
+export default {
+  components: {
+    Header,
+    FooterTwo,
+    BackToTop,
+  },
+};
+</script>
