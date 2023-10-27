f9dfb321e4b919af411131ff56295c4c4e7c2e83
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-27 08:09:10 +0300

    nuxt conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
+  nitro: {
+    preset: 'server'
+  },
+  rootDir: __dirname,
   css: [
     "bootstrap/scss/bootstrap.scss",
     "swiper/css/bundle",
@@ -24,7 +28,7 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
       script: [
         {
-          src: "/bootstrap.bundle.min.js",
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js",
           integrity: "sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2",
           crossorigin: "anonymous",
         },
