542ea54980a393264da94b1914553d609366dbe8
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-10-29 00:34:06 +0300

    scrollbar-thumb

--- app.vue
+++ app.vue
@@ -4,6 +4,23 @@
 
 <script>
 export default {
-  
+
 };
 </script>
+
+<style>
+-moz-scrollbar, ::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+
+
+::-webkit-scrollbar {
+  background-color: #fefdf9;
+}
+
+::-webkit-scrollbar-thumb {
+  background: linear-gradient(-45deg, #4eabf7 1%, #4eabf7 48%, #8de7fb);
+  border: 1px solid #434a52;
+}
+</style>
