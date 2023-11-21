39f41f2b633d26f64e631fb96c48520fad895f88
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2023-11-21 21:34:44 +0300

    terms

--- components/awards/LawyerAwards.vue
+++ components/awards/LawyerAwards.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-xl-2 col-xl-2 col-lg-10 col-md-3">
           <div class="award__wrapper-4">
-            <h3 class="award__title-4">Наши партнёры</h3>
+            <h3 class="award__title-4">{{$t('partners')}}</h3>
           </div>
         </div>
         <div class="col-xxl-4 col-xl-4 col-lg-5 col-md-9">
---
+++ components/policy-terms/TermsArea.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="policy__area pb-120">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-10">
+          <div class="policy__wrapper policy__translate p-relative z-index-1">
+            <div class="policy__item mb-35">
+              <h4 class="policy__meta">Last updated: November 20, 2017</h4>
+              <p>
+                Technologies related to blockchain are subject to supervision and control by various regulatory bodies
+                around the world.
+              </p>
+              <p>SmartHoldem tokens may fall under one or more requests or actions on their part, including but not limited
+                to imposing restrictions on the use or possession of digital tokens such as SmartHoldem that can slow or
+                limit the functionality of SmartHoldem tokens in the future.</p>
+              <p>
+                SmartHoldem tokens are not an investment.
+              </p>
+              <p>
+                SmartHoldem tokens are not a “surrogate currency”.
+              </p>
+              <p>
+                SmartHoldem tokens are not official or legally binding investment. Due to unforeseen circumstances, the
+                objectives described in this document may be amended. Despite the fact that we intend to reach all the
+                points described in this document, all persons and parties involved in the purchase of SmartHoldem tokens
+                do this at their own risk.
+              </p>
+            </div>
+
+            <div class="policy__item policy__item-2 mb-35">
+              <h3 class="policy__title">The risk of using new technologies.</h3>
+              <p>
+                Cryptographic tokens, such as SmartHoldem, are a new and experimental technology. In addition to the
+                risks mentioned in this document, there are additional risks that the SmartHoldem team can not foresee.
+                These risks can materialize in other forms of risk than those specified here.
+              </p>
+            </div>
+
+            <div class="policy__contact">
+              <h3 class="policy__title policy__title-2">Contact us</h3>
+              <p>You may contact us at any time via:</p>
+
+              <ul>
+                <li>
+                  Email:
+                  <span><a href="mailto:harry.com">support@smartholdem.io</a></span>
+                </li>
+              </ul>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
