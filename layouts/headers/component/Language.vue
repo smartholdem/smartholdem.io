c8f3ffa36b65cad8e13826d7034cb8ec9e12198c
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-01-15 04:55:43 +0300

    lang selector

--- layouts/headers/component/Language.vue
+++ layouts/headers/component/Language.vue
@@ -8,8 +8,8 @@
     </span
     >
     <ul :class="`header__lang-list tp-lang-list ${open?'tp-lang-list-open':''}`">
-      <li>Russian</li>
-      <li>English</li>
+      <li><nuxt-link :to="switchLocalePath('ru')">Russian</nuxt-link></li>
+      <li><nuxt-link :to="switchLocalePath('en')">English</nuxt-link></li>
     </ul>
   </span>
 </template>
