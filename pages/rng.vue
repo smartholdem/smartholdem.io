5e007917da5bca40b42965743d64478c8b104fff
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-01-15 07:44:58 +0300

    rng

---
+++ pages/rng.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header-one :header_solid="true" :commonOffcanvas="true" />
+    <RngDetails />
+    <Footer />
+    <back-to-top />
+  </div>
+</template>
+
+<script>
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import RngDetails from "~~/components/blog-details/RngDetails.vue";
+import Footer from "~~/layouts/footers/FooterTwo.vue";
+import BackToTop from "~~/layouts/footers/component/BackToTop.vue";
+
+export default {
+  components: { HeaderOne, Footer, BackToTop, RngDetails },
+  setup() {
+    useHead({
+      title: "Blog Details",
+    });
+  },
+};
+</script>
