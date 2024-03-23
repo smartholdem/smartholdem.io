d285efd2423f0ff9fc5282ab8066697f21d21a9e
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-03-23 23:22:46 +0300

    about decentralize

---
+++ components/breadcrumb/AboutDecentralizeBreadcrumb.vue
@@ -0,0 +1,38 @@
+<template>
+  <section
+    class="breadcrumb__area breadcrumb__style-6 p-relative include-bg pt-200 pb-120"
+  >
+    <div
+      class="breadcrumb__bg-2 breadcrumb__overlay include-bg"
+      :style="{ backgroundImage: `url(${bg})` }"
+    ></div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xxl-8 col-xl-8 col-lg-10">
+          <div class="breadcrumb__content text-center p-relative z-index-1">
+            <h3 class="breadcrumb__title">{{$t('manage_sth')}}</h3>
+            <div class="breadcrumb__list">
+              <span>
+                <nuxt-link :to="localePath('/')">{{$t('home')}}</nuxt-link>
+              </span>
+              <span class="dvdr"><i class="fa-solid fa-circle-small"></i></span>
+              <span>
+                <a target="_blank" href="https://wallet.smartholdem.io/">{{$t('wallet')}}</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import bg from '~/assets/img/sth/about-decentralize/header.png';
+
+export default {
+  data () {
+    return {bg}
+  }
+};
+</script>
---
+++ pages/about-decentralize.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header :top_bar="true" :commonOffcanvas="true" />
+    <decentralize-breadcrumb/>
+    <about-wallet-area/>
+    <Footer :style_2="true"/>
+    <back-to-top/>
+  </div>
+</template>
+
+<script>
+import Header from "~~/layouts/headers/HeaderOne.vue";
+import DecentralizeBreadcrumb from '~~/components/breadcrumb/AboutDecentralizeBreadcrumb.vue';
+import AboutWalletArea from '~~/components/service-details/AboutWalletArea.vue';
+import Footer from '~~/layouts/footers/FooterTwo.vue';
+import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
+
+export default {
+  components: {
+    Header,
+    DecentralizeBreadcrumb,
+    AboutWalletArea,
+    Footer,
+    BackToTop,
+  },
+  setup() {
+    useHead({
+      title: "About Wallet & Assets",
+    });
+  },
+};
+</script>
