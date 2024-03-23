fce6494fcc8d6080fbd53a1823cb135b7a03a60c
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-03-23 05:55:57 +0300

    about wallet

---
+++ pages/about-wallet.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header :top_bar="false" :commonOffcanvas="true" />
+    <wallet-breadcrumb/>
+    <about-wallet-area/>
+    <footer :style_2="true"/>
+    <back-to-top/>
+  </div>
+</template>
+
+<script>
+import Header from "~~/layouts/headers/HeaderNine.vue";
+import WalletBreadcrumb from '~~/components/breadcrumb/AboutWalletBreadcrumb.vue';
+import AboutWalletArea from '~~/components/service-details/AboutWalletArea.vue';
+import Footer from '~~/layouts/footers/FooterTwo.vue';
+import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
+
+export default {
+  components: {
+    Header,
+    WalletBreadcrumb,
+    AboutWalletArea,
+    Footer,
+    BackToTop,
+  },
+  setup() {
+    useHead({
+      title: "About Wallet & Assets",
+    });
+  },
+};
+</script>
