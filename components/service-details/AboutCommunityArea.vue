89293a1ec47711b32ea055a3af8b0a9e2dd31361
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-04-15 05:49:16 +0300

    area

---
+++ components/service-details/AboutCommunityArea.vue
@@ -0,0 +1,246 @@
+<template>
+  <section class="services__area pt-120 pb-125">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="services__widget-2 pr-50">
+            <div class="services__widget-item-2 mb-30">
+              <div class="services__widget-tab-2 tp-tab">
+                <ul>
+                  <li>
+                    <nuxt-link :to="localePath('/about-decentralize')" class="">
+                      <span>
+                        <svg width="22" height="26" viewBox="0 0 22 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                              d="M21 7V19C21 23.8 19.75 25 14.75 25H7.25C2.25 25 1 23.8 1 19V7C1 2.2 2.25 1 7.25 1H14.75C19.75 1 21 2.2 21 7Z"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M13.5117 5.20044H8.51172" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4"
+                                d="M10.9961 21.5196C12.0661 21.5196 12.9336 20.6868 12.9336 19.6596C12.9336 18.6323 12.0661 17.7996 10.9961 17.7996C9.92604 17.7996 9.05859 18.6323 9.05859 19.6596C9.05859 20.6868 9.92604 21.5196 10.9961 21.5196Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      Decentralized
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </nuxt-link>
+                  </li>
+                  <li>
+                    <nuxt-link :to="localePath('/about-decentralize')" class="">
+                      <span>
+                        <svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.2222 1L1 15.6667H12L10.7778 25.4444L23 10.7778H12L13.2222 1Z"
+                                stroke="currentColor"
+                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      Fast
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </nuxt-link>
+                  </li>
+                  <li>
+                    <nuxt-link :to="localePath('/about-decentralize')" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.6906 7.6C21.5132 7.6 22.9906 6.12254 22.9906 4.3C22.9906 2.47746 21.5132 1 19.6906 1C17.8681 1 16.3906 2.47746 16.3906 4.3C16.3906 6.12254 17.8681 7.6 19.6906 7.6Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 13.1006H12" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 17.5H16.4" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M14.2 1H8.7C3.2 1 1 3.2 1 8.7V15.3C1 20.8 3.2 23 8.7 23H15.3C20.8 23 23 20.8 23 15.3V9.8"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      Scalable
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </nuxt-link>
+                  </li>
+                  <li>
+                    <nuxt-link :to="localePath('/about-decentralize')" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.5202 11.652C22.4709 11.652 23.6966 10.5171 22.6071 6.79468C21.8694 4.28656 19.7131 2.13026 17.205 1.39258C13.4826 0.303087 12.3477 1.52877 12.3477 4.47949V7.74799C12.3477 10.5171 13.4825 11.652 15.7523 11.652H19.5202Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M21.4297 14.7162C20.3742 19.9708 15.3353 23.784 9.60407 22.8534C5.30283 22.1611 1.8414 18.6997 1.13777 14.3985C0.218507 8.68995 4.00905 3.65103 9.24091 2.58423"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      Energy Efficient
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </nuxt-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8 order-first order-lg-last">
+          <div class="services__details-wrapper">
+            <h3 class="services__details-title">Децентрализованная сеть для инноваций и роста</h3>
+            <p>
+              Публичная сеть SmartHoldem Level 1 (DPOS) — это блокчейн с открытым исходным кодом, работающий на основе
+              Delegated Proof of Stake (DPoS). SmartHoldem даёт возможность создавать и получать доступ к широкому
+              спектру децентрализованных инструментов, приложений и экосистем.
+            </p>
+
+            <p>SmartHoldem не контролируется какой-либо отдельной организацией, а управляется сообществом владельцев
+              цифровых активов. Если у вас есть хотя бы 1 STH, у Вас есть право голоса в сети!</p>
+            <p>
+              DPoS один из самых экологичных, энергоэффективных и масштабируемых консенсусов, обеспечивающий большее
+              количество быстрых и дешевых транзакций, чем сети PoS и PoW.
+            </p>
+
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/about-community/1.png" alt="community 1"/>
+            </div>
+
+            <p>
+              Консенсус Delegated Proof of Stake (DPoS) основан на демократических принципах, предоставляя сообществу
+              право участвовать в процессах принятия решений, которые определяют, как будет развиваться сеть.
+            </p>
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/about-community/2.png" alt="community 2">
+            </div>
+
+
+            <div class="services__details-text mb-25">
+              <h3 class="services__details-text-title">Делегаты SmartHoldem</h3>
+              <p>
+                Делегаты обеспечивают безопасность блокчейн сети, они проверяют транзакций, создают новые блоки,
+                укрепляют сеть SmartHoldem и получают вознаграждения за каждый подписанный блок. Делегаты не могут
+                изменять информацию о блокчейн транзакциях или как то влиять на них.
+              </p>
+              <p>
+                Делегаты избираются пользователями, которые голосуют своими цифровыми активами. Избранные делегаты
+                представляют держателей монет SmartHoldem, предлагая идеи или изменения в сети и внося свой вклад в
+                улучшение экосистемы.
+              </p>
+              <p>
+                Пользователи голосуют за делегата силой своего голоса. Такой подход избавляет пользователей от
+                необходимости заморозки монет и отправления монет на какой-либо адрес. Монеты остаются на балансе
+                пользователя и под его полным контролем. Каждый держатель STH имеет право голосовать, выбирать или
+                отменять делегатов.
+              </p>
+              <p>
+                В SmartHoldem DPOS подписывают блоки ТОП 21 активный делегат
+                https://blockexplorer.smartholdem.io/#/delegate-monitor, другие зарегистрированные делегаты и узлы
+                находятся в
+                режиме relay node.
+              </p>
+              <p>
+                Система делегирования чрезвычайно конкурентоспособна, поскольку делегат может в любое время перейти в
+                высшую позицию ТОП 21 или покинуть ее.
+                Если сообщество считает, что делегат плохо справляется со своей задачей или кто-то мог бы выполнить ее
+                лучше, его можно заменить.
+              </p>
+              <p>
+                Эта устойчивая схема предоставляет возможность новым делегатам проявить себя, привлечь внимание
+                сообщества и войти в ТОП 21 в любой момент, если сообщество проголосует за нового делегата на активную
+                позицию достаточной силой голоса.
+              </p>
+            </div>
+
+
+            <div class="services__details-text mb-25">
+              <h3 class="services__details-text-title">Как голосовать:</h3>
+              <ul>
+                <li>
+                  Войдите в свой кошелёк SmartHoldem <a  class="text-primary" href="https://wallet.smartholdem.io">wallet.smartholdem.io</a>
+                  или в <a  class="text-primary" href="https://t.me/sthwlt_bot">Telegram APP</a>
+                </li>
+                <li>Нажмите Vote</li>
+                <li>Укажите имя делегата</li>
+                <li>Нажмите "Проголосовать за Делегата"</li>
+              </ul>
+            </div>
+
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/about-community/3.png" alt="delegates vote 3">
+            </div>
+
+            <div class="services__details-text mb-25">
+              <h3 class="services__details-text-title">Вознаграждение Делегатов</h3>
+              <p>Делегат, подписавший блок, получает ВСЕ КОМИССИИ за транзакции, включенные в блок. Различные транзакции
+                имеют свою комиссию в блокчейне SmartHoldem. Например, если блок включает 500 стандартных транзакций,
+                делегат заработает до 500 STH. Если в блоке не было транзакций, делегат ничего не заработает за этот
+                блок. Если блок включает транзакцию регистрации делегата, делегат заработает 10 000 STH. Если блок
+                включает регистрацию контракта или токена, делегат получит от 10 000 STH</p>
+              <p>В блокчейне SmartHoldem Level 1 (DPOS) отсутствует <strong>эмиссия</strong>. Узнать подробнее.
+                Дополнительных вознаграждений кроме комиссий за транзакции, не предусмотрено.</p>
+              <p>
+                Стоимость операций в блокчейне SmartHoldem <a  class="text-primary" href="https://wallet.smartholdem.io/blockchain/">wallet.smartholdem.io/blockchain</a>
+              </p>
+              <p>
+                DPoS блокчейн SmartHoldem включает в себя новые типы транзакций, такие как множественные платежи,
+                игровые транзакции, мультиподписи, криптоподписи, вторую парольную фразу, транзакции сжигания,
+                регистрационные транзакции, хэш-транзакции IPFS, хешированный контракт с временной блокировкой (HTLC),
+                расширенный протокол криптофункции с нулевым разглашением (Zero-Knowledge Proof), отставка делегата,
+                регистрация авторских прав, создание токенов, контактов, ICO, Web-DApp, NFT, регистрация доменных имён,
+                игровые транзакции.
+              </p>
+            </div>
+
+            <div class="services__details-text mb-25">
+              <h3 class="services__details-text-title">Стать делегатом SmartHoldem</h3>
+              <p>Делегатом SmartHoldem может стать любой желающий.</p>
+              <p>Всё, что вам нужно, - это надежный сервер и активное участие в сообществе SmartHoldem. Однако только
+                делегаты, попавшие в число 21 лучших, могут создавать новые блоки и получать вознаграждение в
+                SmartHoldem STH.</p>
+              <p>Начать работу со SmartHoldem очень просто, благодаря пошаговым руководствам и поддержке сообщества для
+                всех, независимо от их технического образования.</p>
+            </div>
+
+            <div class="services__details-text mb-25">
+              <h3 class="services__details-text-title">Связь с сообществом</h3>
+              <p>Для делегата активное участие и связь с сообществом имеют жизненно важное значение для получения
+                голосов.</p>
+              <p>Формирование делегатских слотов является конкурентоспособным и требует большего, чем просто управление
+                узлом. Создание новых приложений на базе SmartHoldem, предложение дополнительных услуг, таких как
+                проверка кода, отчеты об уязвимостях или работа с сообществом, может сыграть решающую роль в голосовании
+                за или против.</p>
+              <p>Для получения необходимых голосов для выхода в ТОП 21 в качестве делегата, вы можете предложить
+                избирателям процент от вознаграждений. Вознаграждение измеряется в зависимости от доли участия
+                избирателя, количества голосов и процента выплат, установленных делегатом.</p>
+              <p>Как стать делегатом и установить собственный узел, следуйте инструкциям на нашем <a class="text-primary" href="https://github.com/smartholdem/sth-core">GitHub</a></p>
+            </div>
+
+            <!-- faq start -->
+            <!--
+            <service-details-faq/>
+            -->
+            <!-- faq end -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ServiceDetailsFaq from '../faqs/ServiceDetailsFaq.vue';
+import ServiceDetailsForm from '../forms/ServiceDetailsForm.vue';
+
+export default {
+  components: {ServiceDetailsFaq, ServiceDetailsForm},
+
+}
+</script>
