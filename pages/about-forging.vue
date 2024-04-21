cb6592d68de1fb7119f43d14fb8aae38a01b3fb4
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-04-21 17:33:55 +0300

    about forging

---
+++ pages/about-forging.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Header :top_bar="true" :commonOffcanvas="true" />
+    <Breadcrumb/>
+    <Area/>
+    <Footer :style_2="true"/>
+    <back-to-top/>
+  </div>
+</template>
+
+<script>
+import Header from "~~/layouts/headers/HeaderOne.vue";
+import Breadcrumb from '~~/components/breadcrumb/AboutForgingBreadcrumb.vue';
+import Area from '~~/components/service-details/AboutForgingArea.vue';
+import Footer from '~~/layouts/footers/FooterTwo.vue';
+import BackToTop from '~~/layouts/footers/component/BackToTop.vue';
+
+export default {
+  components: {
+    Header,
+    Breadcrumb,
+    Area,
+    Footer,
+    BackToTop,
+  },
+  setup() {
+    useHead({
+      title: "Decentralized",
+    });
+  },
+};
+</script>
