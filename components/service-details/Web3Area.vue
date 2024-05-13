ab42139101829f237202a6810d461625b71df697
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-05-13 17:11:43 +0300

    web3 area ru

---
+++ components/service-details/Web3Area.vue
@@ -0,0 +1,286 @@
+<template>
+  <section class="services__area pt-120 pb-125">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="services__widget-2 pr-50">
+            <div class="services__widget-item-2 mb-30">
+              <div class="services__widget-tab-2 tp-tab">
+                <ul>
+                  <li>
+                    <a href="#tokenomics-supply" class="">
+                      <span>
+                        <svg width="22" height="26" viewBox="0 0 22 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                              d="M21 7V19C21 23.8 19.75 25 14.75 25H7.25C2.25 25 1 23.8 1 19V7C1 2.2 2.25 1 7.25 1H14.75C19.75 1 21 2.2 21 7Z"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M13.5117 5.20044H8.51172" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4"
+                                d="M10.9961 21.5196C12.0661 21.5196 12.9336 20.6868 12.9336 19.6596C12.9336 18.6323 12.0661 17.7996 10.9961 17.7996C9.92604 17.7996 9.05859 18.6323 9.05859 19.6596C9.05859 20.6868 9.92604 21.5196 10.9961 21.5196Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('game_platform') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#tkn-deflation" class="">
+                      <span>
+                        <svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.2222 1L1 15.6667H12L10.7778 25.4444L23 10.7778H12L13.2222 1Z"
+                                stroke="currentColor"
+                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('tkn_title2') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#tkn-post" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.6906 7.6C21.5132 7.6 22.9906 6.12254 22.9906 4.3C22.9906 2.47746 21.5132 1 19.6906 1C17.8681 1 16.3906 2.47746 16.3906 4.3C16.3906 6.12254 17.8681 7.6 19.6906 7.6Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 13.1006H12" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 17.5H16.4" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M14.2 1H8.7C3.2 1 1 3.2 1 8.7V15.3C1 20.8 3.2 23 8.7 23H15.3C20.8 23 23 20.8 23 15.3V9.8"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('tkn_post') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#tkn3" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.5202 11.652C22.4709 11.652 23.6966 10.5171 22.6071 6.79468C21.8694 4.28656 19.7131 2.13026 17.205 1.39258C13.4826 0.303087 12.3477 1.52877 12.3477 4.47949V7.74799C12.3477 10.5171 13.4825 11.652 15.7523 11.652H19.5202Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M21.4297 14.7162C20.3742 19.9708 15.3353 23.784 9.60407 22.8534C5.30283 22.1611 1.8414 18.6997 1.13777 14.3985C0.218507 8.68995 4.00905 3.65103 9.24091 2.58423"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('tkn_title3') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-show="$i18n.locale === 'ru'" class="col-lg-8 order-first order-lg-last">
+          <div id="web3-control" class="services__details-wrapper">
+            <h3 class="services__details-title">Получите полный контроль над своей игрой</h3>
+            <h4>
+              Вперёд в будущее!
+            </h4>
+            <p>
+              В SmartHoldem игроки всегда на первом месте! SmartHoldem использует технологию блокчейн для создания
+              справедливых, децентрализованных, прозрачных и неизменяемых Web3 игр.
+            </p>
+            <p>
+              Наш главный приоритет — предоставить игрокам безопасный и приятный игровой процесс, которому можно
+              доверять. Доказуемо честные Web3 игры предлагают больше возможностей, чем традиционные игры, что делает их
+              играми будущего. Такой принцип игр бросает вызов устаревшим игровым механикам и разрушает географические
+              барьеры между игроками.
+            </p>
+            <p>
+              Внедрение принципа доказуемо честных игр SmartHoldem запускает процесс перехода цифровой игровой и
+              азартной индустрии к более честным принципам взаимодействия с огромной аудиторией игроков по всему миру.
+            </p>
+
+            <h3 class="services__details-title">Доказуемо честные Web3 игры</h3>
+            <h4>
+              Блокчейн игры платформы SmartHoldem гарантируют:
+            </h4>
+            <ul>
+              <li>Прозрачность и справедливость игрового процесса</li>
+              <li>Децентрализованную игровую механику</li>
+              <li>Защиту от ботов</li>
+              <li>Хранение игровых транзакций и операций в блокчейне</li>
+              <li>Использование
+                <nuxt-link :to="localePath('/rng')" class="text-primary">генератора истинных случайных чисел</nuxt-link>
+                для доказуемой справедливой случайности
+              </li>
+              <li>Открытый исходный код</li>
+              <li>Блокчейн кошелёк, где никто не может ограничить депозиты или выводы игроков</li>
+              <li>Полное владение игроками своей цифровой собственностью</li>
+            </ul>
+
+            <p class="mt-10">
+              Доказуемо-честные блокейн игры SmartHoldem предоставляют более захватывающий и приятный игровой опыт и
+              дают игрокам больше власти, чем когда-либо прежде.
+            </p>
+
+            <h3 class="services__details-title">SmartHoldem TRNG</h3>
+            <p>
+              Блокчейн повышает эффективность генераторов случайных чисел, обеспечивая 100% случайные, заслуживающие
+              доверия и справедливые результаты в Web3 играх, которые можно проверить с помощью криптографических
+              доказательств.
+            </p>
+            <p>
+              Даже путешественник во времени не будет знать финальный результат раздачи.
+              <nuxt-link :to="localePath('/rng')" class="text-primary">Исследуйте SmartHoldem TRNG</nuxt-link>
+            </p>
+
+
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/web3/TRNG_games.png" alt="True RNG SmartHoldem Games"
+                   title="True RNG SmartHoldem Game">
+            </div>
+
+          </div>
+
+          <div id="web3-2" class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Подход, ориентированный на игрока</h3>
+            <p>
+              Игровой процесс индивидуален, и у каждого игрока есть свои способы получения удовольствия от игры.
+            </p>
+            <p>
+              У игроков всегда есть возможность проверить и доказать, что результаты игр являются справедливыми. Игра
+              защищены от несанкционированного доступа, в них отсутствует контролирующий сервер с заранее известными
+              результатами, и никто не может подтасовать или повлиять на выигрышный шанс.
+            </p>
+            <p>
+              Алгоритм доказательства честных игр SmartHoldem гарантирует равные условия игры для всех, и самое главное
+              - результат был и будет абсолютно случайным и свободным от вмешательства человека, центрального органа
+              власти или третьей стороны.
+            </p>
+          </div>
+
+          <div id="web3-3" class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Web3 экосистема</h3>
+            <p id="web3-3">
+              SmartHoldem представляет глобальную Web3 экосистему, основанную на собственной криптовалюте.
+              <nuxt-link :to="localePath('/tokenomics')" class="text-primary">Узнайте больше о SmartHoldem</nuxt-link>
+            </p>
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/web3/Gamers_ecosystem.png" alt="web3 gamer ecosystem">
+            </div>
+            <ul>
+              <li>
+                Игроки полностью владеют своими активами и закрытыми ключами. Криптовалюта платформы SmartHoldem STH
+                обеспечивает быстрый доступ к выигрышам, что делает депозиты и снятие средств проще, чем когда-либо
+                прежде.
+              </li>
+              <li>
+                Децентрализация позволяет игрокам управлять кошельками независимо от кого-либо. Криптовалюта и блокчейн
+                SmartHoldem устраняют барьеры для входа, обеспечивая глобальный доступ без границ.
+              </li>
+              <li>
+                SmartHoldem защищает конфиденциальность пользователей. Для входа в любую игру на платформе, Вам нужно
+                создать кошелек в блокчейне, для это не требуется проходить KYC или AML. Приватную фразу, выданную
+                блокчейном при регистрации, сохраните в надежном месте. Никто кроме вас не хранит вашу парольную фразу.
+                Доступ к вашему аккаунту есть только у вас!
+              </li>
+            </ul>
+            <h4 class="mt-10">
+              <a href="https://wallet.smartholdem.io" class="text-primary"> Создать кошелек SmartHoldem</a>
+            </h4>
+            <h4>
+              Вы можете использовать свой крипто-адрес как единый игровой адрес для входа в любую игру на платформе
+              SmartHoldem.
+            </h4>
+            <ul>
+              <li>
+                Блокчейн SmartHoldem записывает все игровые транзакции, позволяя игрокам проверять результаты раздач,
+                лотерей, розыгрышей и много другого.
+              </li>
+              <li>
+                Уникальные акции и аирдропы обеспечивают дополнительные стимулы для игроков, даря приятные эмоции.
+              </li>
+              <li>
+                Игровая статистика позволяет отслеживать игровой прогресс, количество сыгранных игр, процент побед,
+                достигнутых уровней и т.д., повышая доверие между игроками.
+              </li>
+            </ul>
+          </div>
+
+          <div id="web3-partner" class="services__details-thumb m-img">
+            <img src="~/assets/img/sth/web3/Affiliate_ru.png" alt="web3 blockchain affiliate partner program"
+                 title="in-chain partner program">
+          </div>
+
+          <div id="web3-4" class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Первая в мире партнерская программа на блокчейне</h3>
+            <p>
+              В блокчейне SmartHoldem реализована первая в мире партнерская программа BRO-chain, полностью работающая на
+              блокчейне.
+            </p>
+            <p>
+              Используя возможности технологии блокчейн, мы создали безопасную, эффективную и прозрачную программу. С
+              нашей партнерской программой вы можете получать вознаграждения за продвижение SmartHoldem в вашей сети.
+              Присоединяйтесь к нам сейчас и станьте частью будущего партнерского маркетинга!
+            </p>
+            <p>
+              Ваш SmartHoldem-адрес — это ваш уникальный партнерский код. Каждый раз, когда ваши друзья используют вашу
+              реферальную ссылку в игре и выигрывают, вы получаете 1% от их выигрыша, а они получают дополнительно 1% к
+              их выигрышу. Это Win-Win предложение для всех участников!
+            </p>
+            <p>
+              Партнёрская программа SmartHoldem позволяет вам получать вознаграждения, делясь своей реферальной ссылкой
+              с другими игроками. Все платежи и регистрации фиксируются в блокчейне, обеспечивая 100% учёт всех
+              рефералов и платежей.
+            </p>
+            <p>
+              Присоединяйтесь к нашей партнерской программе, чтобы получать неограниченные мгновенные выплаты, отчеты в
+              реальном времени, децентрализованный блокчейн-кошелек и надежный сервис.
+            </p>
+          </div>
+
+          <div id="web3-5" class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Откройте новые горизонты с играми Web3</h3>
+            <p>
+              Геймеры Web3 — это растущее сообщество людей, заинтересованных в доказуемо-честных блокчейн играх, которые
+              дарят захватывающие и превосходные эмоции. Web3 игры имеют ряд преимуществ над традиционными играми, такие
+              как: безопасность, прозрачность, децентрализация, независимость и 100% владение игровыми предметами.
+            </p>
+            <p>
+              Исследуйте блокчейн инновации SmartHoldem и специализированные решения для интеграции Web3 в игровую и iGaming индустрию.
+            </p>
+          </div>
+
+
+        </div>
+      </div>
+
+
+    </div>
+
+  </section>
+</template>
+
+<script>
+import ServiceDetailsFaq from '../faqs/ServiceDetailsFaq.vue';
+import ServiceDetailsForm from '../forms/ServiceDetailsForm.vue';
+
+export default {
+  components: {ServiceDetailsFaq, ServiceDetailsForm},
+
+}
+</script>
