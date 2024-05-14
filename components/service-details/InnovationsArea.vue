4538ba025af80f376c08273bb05d6681b7e0bc79
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-05-14 13:06:10 +0300

    innovations

---
+++ components/service-details/InnovationsArea.vue
@@ -0,0 +1,270 @@
+<template>
+  <section class="services__area pt-120 pb-125">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="services__widget-2 pr-50">
+            <div class="services__widget-item-2 mb-30">
+              <div class="services__widget-tab-2 tp-tab">
+                <ul>
+                  <li>
+                    <a href="#innovations" class="">
+                      <span>
+                        <svg width="22" height="26" viewBox="0 0 22 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                              d="M21 7V19C21 23.8 19.75 25 14.75 25H7.25C2.25 25 1 23.8 1 19V7C1 2.2 2.25 1 7.25 1H14.75C19.75 1 21 2.2 21 7Z"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M13.5117 5.20044H8.51172" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4"
+                                d="M10.9961 21.5196C12.0661 21.5196 12.9336 20.6868 12.9336 19.6596C12.9336 18.6323 12.0661 17.7996 10.9961 17.7996C9.92604 17.7996 9.05859 18.6323 9.05859 19.6596C9.05859 20.6868 9.92604 21.5196 10.9961 21.5196Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('web3_1') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#web3-fair" class="">
+                      <span>
+                        <svg width="22" height="26" viewBox="0 0 22 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path
+                              d="M21 7V19C21 23.8 19.75 25 14.75 25H7.25C2.25 25 1 23.8 1 19V7C1 2.2 2.25 1 7.25 1H14.75C19.75 1 21 2.2 21 7Z"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M13.5117 5.20044H8.51172" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4"
+                                d="M10.9961 21.5196C12.0661 21.5196 12.9336 20.6868 12.9336 19.6596C12.9336 18.6323 12.0661 17.7996 10.9961 17.7996C9.92604 17.7996 9.05859 18.6323 9.05859 19.6596C9.05859 20.6868 9.92604 21.5196 10.9961 21.5196Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('web3_fair') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#web3-2" class="">
+                      <span>
+                        <svg width="24" height="27" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13.2222 1L1 15.6667H12L10.7778 25.4444L23 10.7778H12L13.2222 1Z"
+                                stroke="currentColor"
+                                stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      SmartHoldem TRNG
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#web3-3" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.6906 7.6C21.5132 7.6 22.9906 6.12254 22.9906 4.3C22.9906 2.47746 21.5132 1 19.6906 1C17.8681 1 16.3906 2.47746 16.3906 4.3C16.3906 6.12254 17.8681 7.6 19.6906 7.6Z"
+                                stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 13.1006H12" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path opacity="0.4" d="M6.5 17.5H16.4" stroke="currentColor" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M14.2 1H8.7C3.2 1 1 3.2 1 8.7V15.3C1 20.8 3.2 23 8.7 23H15.3C20.8 23 23 20.8 23 15.3V9.8"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('web3_3') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#web3-partner" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.5202 11.652C22.4709 11.652 23.6966 10.5171 22.6071 6.79468C21.8694 4.28656 19.7131 2.13026 17.205 1.39258C13.4826 0.303087 12.3477 1.52877 12.3477 4.47949V7.74799C12.3477 10.5171 13.4825 11.652 15.7523 11.652H19.5202Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M21.4297 14.7162C20.3742 19.9708 15.3353 23.784 9.60407 22.8534C5.30283 22.1611 1.8414 18.6997 1.13777 14.3985C0.218507 8.68995 4.00905 3.65103 9.24091 2.58423"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('web3_4') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#web3-5" class="">
+                      <span>
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path opacity="0.4"
+                                d="M19.5202 11.652C22.4709 11.652 23.6966 10.5171 22.6071 6.79468C21.8694 4.28656 19.7131 2.13026 17.205 1.39258C13.4826 0.303087 12.3477 1.52877 12.3477 4.47949V7.74799C12.3477 10.5171 13.4825 11.652 15.7523 11.652H19.5202Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                          <path
+                              d="M21.4297 14.7162C20.3742 19.9708 15.3353 23.784 9.60407 22.8534C5.30283 22.1611 1.8414 18.6997 1.13777 14.3985C0.218507 8.68995 4.00905 3.65103 9.24091 2.58423"
+                              stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                      </span>
+                      {{ $t('web3_5') }}
+                      <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 11L6 6L1 1" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                              stroke-linejoin="round"/>
+                      </svg>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-if="$i18n.locale === 'ru'" class="col-lg-8 order-first order-lg-last">
+          <div class="services__details-wrapper">
+            <h3 class="services__details-text-title">Индустрия на миллиарды долларов</h3>
+            <p>
+              Игровая индустрия в настоящее время переживает значительную трансформацию благодаря появлению технологии
+              блокчейна и Web3. Эта мощная комбинация разрушает барьеры, открывает новые возможности и дает игрокам
+              больше власти, чем когда-либо прежде.
+            </p>
+            <p>
+              На данный момент более 3 миллиардов человек по всему миру играют в онлайн игры. По состоянию на 2023 год,
+              более 420 миллионов пользователей по всему миру владеют криптовалютами. Переход из обычного геймера в
+              крипто геймера произойдет в ближайшее время, и для этого есть причины.
+            </p>
+            <p id="web3-fair">
+              На смену видео играм приходят Web3 игры на блокчейне. Игроки хотят играть в блокчейн-игры, чтобы
+              заработать деньги и получать реальные награды. Такая мотивация фундаментально меняет правила игровой
+              индустрии, способы создания, распространения и монетизации игр.
+            </p>
+
+            <h3 class="services__details-text-title">Рост только начинается</h3>
+
+            <p>
+              <a target="_blank" class="text-primary"
+                 href="https://www.grandviewresearch.com/industry-analysis/blockchain-gaming-market-report">По
+                статистике</a>
+              глобальный рынок блокчейн игр, по прогнозам, к 2027 году вырастет до 65,7 миллиардов долларов, а к 2030
+              году доход игрового блокчейн сектора достигнет 300 миллиардов долларов. Средне-годовой темп роста 68,3% с
+              2023 по 2030 год. Рост рынка напрямую связан с востребованностью в безопасной, прозрачной и
+              децентрализованной игровой экосистеме. Массовое внедрение технологичных блокчейн Web3 игр и платформ для
+              их создания повлияет на статистику и может показать еще большую прибыльность, которую на данный момент
+              сложно оценить.
+            </p>
+
+            <div class="services__details-thumb m-img">
+              <img src="~/assets/img/sth/innovation/us-blockchain-in-gaming-market.png"
+                   alt="US Blockchain in Gaming Market 2020-2030"
+                   title="Blockchain in Gaming Market 2020-2030">
+            </div>
+          </div>
+
+          <div class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Будущее Инноваций</h3>
+            <p>
+              Децентрализованная платформа SmartHoldem — это комплексное универсальное Web3 блокчейн решение для
+              разработчиков Web3 игр, игровых провайдеров и операторов онлайн-казино, ставок, покер-румов.
+            </p>
+            <p>
+              Блокчейн SmartHoldem предлагает широкий функционал для создания высококачественных и
+              высокопроизводительных игровых приложений нового поколения, способствующих улучшению качества игрового
+              процесса, повышению вовлеченности и росту бизнеса.
+            </p>
+          </div>
+
+          <div class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Игрокам</h3>
+            <p>
+              Игроки получают возможность играть в доказуемо-честные Web3 игры, совершать быстрые и безопасные
+              транзакции, владеть своими цифровыми активами, торговать на биржах и испытать полную игровую свободу в
+              один клик мыши. Узнайте больше о доказуемо-честных Web3 играх (ссылка на “про игры”)
+            </p>
+          </div>
+
+          <div class="services__details-thumb m-img">
+            <img src="~/assets/img/sth/web3/Affiliate_ru.png" alt="web3 blockchain affiliate partner program"
+                 title="in-chain partner program">
+          </div>
+
+          <div id="web3-4" class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Разработчикам</h3>
+            <p>
+              Разработчики получают эффективные блокчейн инструменты для различных сценариев использования, в том числе
+              по созданию игр полностью работающих на блокчейне. Такие проекты вызывают повышенный интерес у игроков и
+              инвесторов, поэтому лучшее время для создания Web3 приложений сейчас как никогда ранее.
+            </p>
+            <p>
+              Создавайте и разрабатывайте Web3 игры и приложения на самых популярных языках программирования. Платформа
+              SmartHoldem поддерживает JavaScript, TypeScript и Python.
+            </p>
+            <p>Набор инструментов для создания масштабируемых блокчейн игр включает в себя: </p>
+            <ul>
+              <li><a class="text-primary" target="_blank" href="https://wiki.smartholdem.io/">API для разработки</a>
+                Web3 игр на блокчейне
+              </li>
+              <li>Понятный SDK</li>
+              <li>Модуль кошелька</li>
+              <li>Генератор истинно случайных чисел</li>
+              <li>
+                Различные типы игровых транзакций такие как множественные платежи, мультиподписи, криптоподписи,
+                хэш-транзакции IPFS, хешированный контракт с временной блокировкой (HTLC), расширенный протокол
+                криптофункции с нулевым разглашением (Zero-Knowledge Proof), регистрация авторских прав, выпуск игровых
+                токенов, NFTs.
+              </li>
+              <li>Тестирование игровых функций в тестовой сети</li>
+            </ul>
+            <h4>Создайте свою первую Web3 игру на SmartHoldem. </h4>
+            <a class="text-primary" target="_blank" href="https://github.com/smartholdem">Пример по разработке простой Web3 игры с TRNG SmartHoldem.</a>
+
+          </div>
+
+          <div class="services__details-text mb-25">
+            <h3 class="services__details-text-title">Откройте новые горизонты с играми Web3</h3>
+            <p>
+              Геймеры Web3 — это растущее сообщество людей, заинтересованных в доказуемо-честных блокчейн играх, которые
+              дарят захватывающие и превосходные эмоции. Web3 игры имеют ряд преимуществ над традиционными играми, такие
+              как: безопасность, прозрачность, децентрализация, независимость и 100% владение игровыми предметами.
+            </p>
+            <p>
+              Исследуйте блокчейн инновации SmartHoldem и специализированные решения для интеграции Web3 в игровую и
+              iGaming индустрию.
+            </p>
+          </div>
+
+
+        </div>
+
+        <!-- SmartHoldem WEB3 English -->
+
+      </div>
+
+
+    </div>
+
+  </section>
+</template>
+
+<script>
+import ServiceDetailsFaq from '../faqs/ServiceDetailsFaq.vue';
+import ServiceDetailsForm from '../forms/ServiceDetailsForm.vue';
+
+export default {
+  components: {ServiceDetailsFaq, ServiceDetailsForm},
+
+}
+</script>
