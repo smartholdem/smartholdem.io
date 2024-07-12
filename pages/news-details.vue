7ded88bfeee837b4a2d6a609647b36f657a1d9f1
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-07-12 23:35:31 +0300

    news detail

---
+++ pages/news-details.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header-one :header_solid="true" :commonOffcanvas="true" />
+    <blog-details-area-two />
+    <Footer />
+    <back-to-top />
+  </div>
+</template>
+
+<script>
+import HeaderOne from "~~/layouts/headers/HeaderOne.vue";
+import BlogDetailsAreaTwo from "~~/components/blog-details/BlogDetailsAreaTwo.vue";
+import Footer from "~~/layouts/footers/FooterTwo.vue";
+import BackToTop from "~~/layouts/footers/component/BackToTop.vue";
+
+export default {
+  components: { HeaderOne, Footer, BackToTop, BlogDetailsAreaTwo },
+  setup() {
+    useHead({
+      title: "News Details",
+    });
+  },
+};
+</script>
