06e00be9084850d1591bab3208f823948253862e
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-07-12 23:33:09 +0300

    mobile wallet

--- layouts/headers/HeaderNine.vue
+++ layouts/headers/HeaderNine.vue
@@ -14,7 +14,7 @@
                   </nuxt-link>
                   <span class="text-white ml-10 pl-5 pr-5 pt-1 pb-1 " title="English language"><nuxt-link :to="switchLocalePath('en')">ENG</nuxt-link></span>
                   <span class="text-white ml-10 pl-5 pt-1 pb-1 " title="Русский язык"><nuxt-link :to="switchLocalePath('ru')">RUS</nuxt-link></span>
-                  <span v-show="$root['isMobile']" class="text-white ml-10 pl-5 pr-5 pt-1 pb-1"><a target="_blank" class="text-warning text-uppercase" href="https://wallet.smartholdem.io/"><i class="fa icon_wallet"></i> <span >{{$t('wallet')}}</span></a></span>
+                  <span v-show="isMobile" class="text-white ml-10 pl-5 pr-5 pt-1 pb-1"><a target="_blank" class="text-warning text-uppercase" href="https://wallet.smartholdem.io/"><i class="fa icon_wallet"></i> <span >{{$t('wallet')}}</span></a></span>
                 </div>
               </div>
               <div class="col-xxl-6 col-xl-8 col-lg-8 d-none d-lg-block">
@@ -79,6 +79,7 @@ export default {
   data() {
     return {
       isSticky: false,
+      isMobile: false,
     };
   },
   methods: {
@@ -94,7 +95,7 @@ export default {
     },
   },
   mounted() {
-    this.$root.isMobile = window.innerWidth < 800 || window.width < 800;
+    this.isMobile = window.innerWidth < 800 || window.width < 800;
     window.addEventListener("scroll", this.handleSticky);
   },
 }
