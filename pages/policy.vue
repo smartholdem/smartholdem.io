e877b4a48194d0a70124b3cbff4be819cb5ae474
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-07-13 02:56:43 +0300

    blog details

---
+++ pages/policy.vue
@@ -0,0 +1,302 @@
+<template xmlns="http://www.w3.org/1999/html">
+  <Layout>
+
+    <section>
+      <div v-show="$i18n.locale === 'ru'" class="container mt-70">
+        <h1 class="text-center">Манифест SmartHoldem v1.0</h1>
+        <h5>
+          Расширение прав и возможностей человечества посредством фундаментальных технологий блокчейна.
+        </h5>
+        <p>
+          Манифест SmartHoldem представляет наше стремление построить лучший мир с помощью технологий, где технология
+          блокчейн является эффективным методом горизонтального сотрудничества в условиях торговли и обмена, реализации
+          и
+          новаторства, солидарности и взаимопомощи.
+        </p>
+        <p>
+          В SmartHoldem мы представляем мир, в котором преобразующий потенциал технологии блокчейн выходит за рамки
+          инноваций в цифровой сфере и формирует более справедливое и устойчивое будущее для человечества. Наш манифест
+          является свидетельством нашей приверженности этому видению, руководствуясь фундаментальными принципами,
+          лежащими
+          в основе технологий блокчейна SmartHoldem.
+        </p>
+        <h5>Человечество и Прогресс</h5>
+        <p>
+          Наша цивилизация процветает благодаря технологиям, которые являются вершиной человеческих достижений и
+          движущей
+          силой прогресса и реализации потенциала. У нас есть инструменты, системы, идеи и воля для достижения
+          значительного улучшения образа жизни.
+        </p>
+        <p>
+          Для процветания общества оно должно постоянно расти и развиваться. Рост означает прогресс, расширение знаний и
+          улучшение благосостояния. Отсутствие роста пагубно и ведет к застою мышления, конфликтам, упадку и, в конечном
+          итоге, к гибели. Есть три источника роста: увеличение населения, использование ресурсов и технологии. Учитывая
+          ограничения первых двух, технологии являются единственным вечным источником роста.
+        </p>
+        <p>
+          В действительности, технологии всегда были основной движущей силой роста. Технологический прогресс измеряется
+          ростом производительности, который позволяет нам производить больше с меньшими ресурсами, принося пользу всему
+          населению и стимулируя экономический рост, рост благосостояния и, в конечном итоге, к достижению изобилия. С
+          материальным изобилием появляется потенциал увеличения населения планеты, что приводит к еще большему
+          количеству
+          инноваций и прогресса.
+        </p>
+        <p>
+          Развитие нашей цивилизации тесно связано с технологическим прогрессом. Именно по этой причине мы больше не
+          живем
+          в примитивных условиях, но имеем потенциал колонизировать другие планеты.
+        </p>
+        <p>
+          Мы твердо верим, что любую проблему, будь то природную или техногенную, можно решить с помощью прогрессивных
+          технологий.
+        </p>
+        <p>
+          В сфере экономики мы выступаем за свободный рынок как наиболее эффективный способ организации экономических
+          процессов. Рынки способствуют инновациям, эффективному распределению ресурсов и дисциплине, обеспечивая
+          обратную
+          связь и стимулы для производителей и потребителей.
+        </p>
+        <p>
+          Интеллект — главный двигатель прогресса, и с интеграцией искусственного интеллекта человечество получает
+          изобретательного помощника. Синергия между людьми и машинным интеллектом, обещает экспоненциальный рост
+          человеческих возможностей.
+        </p>
+        <h5>Блокчейн как расширение прав и возможностей</h5>
+        <p>
+          Блокчейн — это не просто технология. Это инструмент. Мы создаем децентрализованные блокчейн-решения, которые
+          расширяют возможности людей, предоставляя им полный контроль над своими цифровыми активами, данными и
+          финансовыми транзакциями.
+        </p>
+        <p>
+          Цифровая конфиденциальность — это фундаментальное право человека, и наши технологии защищают его. Каждый
+          человек
+          имеет контроль над своей цифровой идентичностью. Мы верим в децентрализацию как катализатор создания более
+          равноправного и справедливого общества.
+        </p>
+        <p>
+          Приверженность децентрализации гарантирует равные возможности для всего человечества.
+        </p>
+        <h5>Открытый исходный код в инновациях</h5>
+        <p>
+          Инновации процветают в открытых экосистемах. Приверженность SmartHoldem принципам открытого исходного кода
+          поощряет сотрудничество, прозрачность и развитие, управляемое сообществом. Благодаря открытому исходному коду
+          мы
+          работаем вместе над решением сложных проблем, консолидируя и обогащая наши коллективные знания. Мы верим в
+          коллективный разум и общий прогресс.
+        </p>
+        <h5>Устойчивое развитие зеленого будущего</h5>
+        <p>
+          Мы признаем острую необходимость в экологически ответственных технологиях блокчейна. SmartHoldem использует
+          устойчивые механизмы консенсуса Delegated Proof-of-Stake (DpoS) и Proof of Spacetime (PoSt) для минимизации
+          потребления энергии и уменьшения углеродного следа блокчейна. Наша приверженность устойчивому развитию выходит
+          за рамки технологий и распространяется на нашу общую ответственность за защиту планеты.
+        </p>
+        <h5>Доступность</h5>
+        <p>
+          Технология блокчейн должна быть доступна всем, независимо от технических знаний, финансового статуса или
+          географического положения. SmartHoldem работает над тем, чтобы сделать преимущества блокчейна доступными для
+          глобальной аудитории, включая следующий миллиард пользователей Web3. Мы уделяем приоритетное внимание созданию
+          удобных интерфейсов и комплексных образовательных ресурсов, гарантируя, что преобразующая сила блокчейна
+          достигнет каждого уголка мира.
+        </p>
+        <h5>Расширение возможностей экономики создателей</h5>
+        <p>
+          SmartHoldem предвидит будущее, в котором процветает экономика создателей, поддерживаемая возможностями
+          блокчейна. С помощью децентрализованного финансирования, DeFi, GameFi и NFT арт-пространств мы стремимся
+          предоставить авторам инструменты для монетизации их талантов и творчества, освобождая их от ограничений
+          централизованных платформ.
+        </p>
+        <h5>Ворота в открытую метавселенную</h5>
+        <p>
+          Блокчейн SmartHoldem служит базовой инфраструктурой открытой метавселенной. Мы верим в будущее, в котором
+          цифровой и физический миры объединятся, обеспечивая плавный переход между ними. Возможности блокчейна
+          SmartHoldem предназначены для объединения этих сфер и поддержки взаимосвязанной метавселенной.
+        </p>
+        <h5>Приверженность этическим нормам</h5>
+        <p>
+          SmartHoldem придерживается самых высоких этических стандартов во всех начинаниях. Мы стремимся к прозрачности,
+          безопасности и конфиденциальности, гарантируя, что наши технологии расширяют возможности людей, не ставя под
+          угрозу их права и ценности.
+        </p>
+        <h5>Сотрудничество и инновации</h5>
+        <p>
+          В духе сотрудничества и инноваций SmartHoldem открыт для сотрудничества с разработчиками, создателями и
+          организациями, чтобы способствовать росту децентрализованных приложений. Через SmartHoldem Labs мы предлагаем
+          финансирование, ресурсы и наставничество инноваторам, которые разделяют наше видение децентрализованного
+          будущего.
+        </p>
+        <h5>Сообщество SmartHoldem</h5>
+        <p>
+          Сообщество SmartHoldem - это открытый клуб визионеров нового времени, разработчиков, предпринимателей,
+          трейдеров, инноваторов, художников,холдеров, игроков которые разделяют идеи децентрализации и заинтересованы в
+          развитии.
+        </p>
+        <p>
+          Мы считаем, что обучение новым технологиям, практическая работа, приобретение полезных навыков, самовыражение,
+          стремление построить что-то большее, чем вы сами, стремление работать с другими, чтобы построить что-то
+          большее
+          в группе, это необходимость для успешного развития в эру цифровых технологий нового времени.
+        </p>
+        <p>
+          Мы верим в сознательность и уникальность людей, присоединившихся к платформе SmartHoldem. Мы считаем, что
+          развитие человеческой цивилизации — одно из самых полезных дел, которые мы все вместе можем сделать.
+        </p>
+        <h5>Заключение</h5>
+        <p>
+          Манифест SmartHoldem является свидетельством нашей непоколебимой приверженности фундаментальным технологиям
+          блокчейна и нашего видения более светлого и более справедливого будущего для человечества.
+        </p>
+        <p>
+          Мы приглашаем всех, кто разделяет наши ценности, присоединиться к нам в этом путешествии, поскольку вместе мы
+          прокладываем путь к децентрализованному, устойчивому миру, полному безграничных возможностей.
+        </p>
+        <p>
+          <strong>Вместе мы меняем реальность.</strong>
+        </p>
+        <hr/>
+        <p>
+          27.10.2023<br>
+          TechnoL0g & Europa
+        </p>
+      </div>
+
+      <div v-show="$i18n.locale === 'en'" class="container mt-70">
+        <h1 class="text-center">SmartHoldem Manifesto v1.0</h1>
+        <h5>
+          Empowering humanity through fundamental blockchain technologies.
+        </h5>
+        <p>
+          The SmartHoldem Manifesto represents our commitment to build a better world through technology, where
+          blockchain technology is an effective method of horizontal collaboration in an environment of commerce and
+          exchange, fulfillment and innovation, solidarity and mutual aid.
+        </p>
+        <p>
+          At SmartHoldem, we envision a world where the transformative potential of blockchain technology goes beyond
+          digital innovation to shape a more equitable and sustainable future for humanity. Our manifesto is a testament
+          to our commitment to this vision, guided by the fundamental principles underpinning SmartHoldem's blockchain
+          technologies.
+
+        </p>
+        <h5>Humanity & Progress</h5>
+        <p>
+          Our civilization thrives on technology, which is the pinnacle of human achievement and the driving force
+          behind progress and the realization of potential. We have the tools, systems, ideas and will to achieve
+          significant improvements in the way we live.
+        </p>
+        <p>
+          For a society to thrive, it must continually grow and evolve. Growth means progress, increased knowledge and
+          improved well-being. Lack of growth is detrimental and leads to stagnant thinking, conflict, decline and
+          ultimately death. There are three sources of growth: population growth, resource utilization, and technology.
+          Given the limitations of the first two, technology is the only perennial source of growth.
+        </p>
+        <p>
+          In fact, technology has always been the primary driver of growth. Technological progress is measured by
+          productivity growth, which allows us to produce more with fewer resources, benefiting the entire population
+          and stimulating economic growth, increased wealth, and ultimately to the attainment of abundance. With
+          material abundance comes the potential to increase the world's population, leading to even more innovation and
+          progress.
+        </p>
+        <p>
+          The development of our civilization is closely tied to technological advancement. It is for this reason that
+          we no longer live in primitive conditions, but have the potential to colonize other planets.
+        </p>
+        <p>
+          We firmly believe that any problem, whether natural or man-made, can be solved with the help of progressive
+          technology.
+        </p>
+        <h5>Blockchain as empowerment</h5>
+        <p>
+          Blockchain is not just a technology. It's a tool. We create decentralized blockchain solutions that empower
+          people by giving them full control over their digital assets, data and financial transactions.
+        </p>
+        <p>
+          Digital privacy is a fundamental human right, and our technology protects it. Every individual has control
+          over their digital identity. We believe in decentralization as a catalyst for a more equal and just society.
+        </p>
+        <p>
+          A commitment to decentralization guarantees equal opportunities for all humanity.
+        </p>
+        <h5>Open source in innovation</h5>
+        <p>
+          Innovation thrives in open ecosystems. SmartHoldem's commitment to open source encourages collaboration,
+          transparency, and community-driven development. Through open source, we work together to solve complex
+          problems, consolidating and enriching our collective knowledge. We believe in collective intelligence and
+          shared progress.
+        </p>
+        <h5>Sustaining a green future</h5>
+        <p>
+          We recognize the critical need for environmentally responsible blockchain technologies. SmartHoldem uses
+          sustainable Delegated Proof-of-Stake (DpoS) and Proof of Spacetime (PoSt) consensus mechanisms to minimize
+          energy consumption and reduce the blockchain's carbon footprint. Our commitment to sustainability goes beyond
+          technology and extends to our shared responsibility to protect the planet.
+        </p>
+        <h5>Accessibility</h5>
+        <p>
+          Blockchain technology should be accessible to everyone, regardless of technical expertise, financial status or
+          geographic location. SmartHoldem is working to make the benefits of blockchain accessible to a global
+          audience, including the next billion Web3 users. We are prioritizing the creation of user-friendly interfaces
+          and comprehensive educational resources, ensuring that the transformative power of blockchain reaches every
+          corner of the world.
+        </p>
+        <h5>Empowering the maker economy</h5>
+        <p>
+          SmartHoldem envisions a future in which the creator economy thrives, supported by the power of blockchain.
+          Through decentralized funding, DeFi, GameFi and NFT art spaces, we aim to provide creators with the tools to
+          monetize their talents and creativity, freeing them from the constraints of centralized platforms.
+        </p>
+        <h5>Gateway to an open meta-universe</h5>
+        <p>
+          The SmartHoldem blockchain serves as the underlying infrastructure of the open meta-universe. We believe in a
+          future where the digital and physical worlds will merge, providing a seamless transition between the two. The
+          capabilities of the SmartHoldem blockchain are designed to unify these spheres and support an interconnected
+          meta-universe.
+        </p>
+        <h5>Commitment to ethical standards</h5>
+        <p>
+          SmartHoldem adheres to the highest ethical standards in all endeavors. We are committed to transparency,
+          security and privacy, ensuring that our technology empowers people without compromising their rights and
+          values.
+        </p>
+        <h5>Collaboration and Innovation</h5>
+        <p>
+          In the spirit of collaboration and innovation, SmartHoldem is open to working with developers, creators, and
+          organizations to foster the growth of decentralized applications. Through SmartHoldem Labs, we offer funding,
+          resources, and mentorship to innovators who share our vision of a decentralized future.
+        </p>
+        <h5>SmartHoldem Community</h5>
+        <p>
+          SmartHoldem Community is an open club of new age visionaries, developers, entrepreneurs, traders, innovators, artists, holders, players who share the ideas of decentralization and are interested in development.
+        </p>
+        <p>
+          We believe that learning new technologies, hands-on work, learning useful skills, self-expression, striving to build something bigger than yourself, striving to work with others to build something bigger as a group, is a necessity for successful development in the new age digital era.
+        </p>
+        <p>
+          We believe in the consciousness and uniqueness of people joining SmartHoldem platform. We believe that the development of human civilization is one of the most useful things we can all do together.
+        </p>
+        <h5>Conclusion</h5>
+        <p>
+          The SmartHoldem Manifesto is a testament to our unwavering commitment to fundamental blockchain technology and our vision of a brighter and more equitable future for humanity.
+        </p>
+        <p>
+          We invite everyone who shares our values to join us on this journey as together we pave the way to a decentralized, sustainable world full of limitless possibilities.
+        </p>
+        <p>
+          <strong>Together we are changing reality</strong>
+        </p>
+        <hr/>
+        <p>
+          10/27/2023<br>
+          TechnoL0g & Europa
+        </p>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script setup>
+import Layout from "~/layouts/LayoutTwo.vue";
+
+useHead({
+  title: "SmartHoldem Manifest",
+});
+</script>
