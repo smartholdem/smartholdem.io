0314a0f754b7b25aa93df382d96c8e058d3b8f43
TechnoL0g <9394904+technologiespro@users.noreply.github.com>
2024-07-13 02:57:12 +0300

    home

---
+++ pages/home.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout>
+    <SliderHome/>
+    <Transform/>
+    <FunFact/>
+    <FunFactB/>
+    <History/>
+    <Video/>
+    <About/>
+    <SectionCommunity/>
+    <!--<Blogs/>-->
+    <Manifest/>
+    <Projects/>
+    <Counter/>
+    <Testimonial/>
+    <Partners/>
+  </Layout>
+</template>
+
+<script setup>
+import Layout from '~/layouts/LayoutTwo.vue';
+//import Layout from "~/layouts/LayoutNine.vue";
+import SliderHome from "~/components/hero-banner/SliderHome.vue";
+//import Banner from '~/components/hero-banner/LawyerBanner.vue';
+import Partners from '~/components/awards/SectionPartners.vue';
+import SectionCommunity from '~/components/portfolios/SectionCommunity.vue';
+import Manifest from '~/components/abouts/Manifesto.vue';
+import Transform from '~/components/abouts/Transform.vue';
+import Video from '~/components/video-area/LawyerVideo.vue';
+import FunFact from '~/components/fun-facts/LawyerFunFact.vue';
+import FunFactB from '~/components/fun-facts/FunFactB.vue';
+import History from '~/components/history/LawyerHistory.vue';
+import About from '~/components/services/About.vue';
+//import Blogs from '~/components/blogs/LawyerBlogs.vue';
+import Testimonial from '~/components/testimonials/LawyerTestimonial.vue';
+import Projects from '~/components/teams/LawyerTeams.vue';
+import Counter from '~~/components/counters/Counter.vue';
+
+
+useHead({
+  title: "SmartHoldem Platform",
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    {
+      hid: 'description',
+      name: 'description',
+      content: 'Decentralized gaming platform SmartHoldem on Blockchain with True RNG'
+    },
+    {
+      hid: 'og:image',
+      name: 'og:image',
+      content: 'https://smartholdem.io/_nuxt/slider-1.e74227fc.jpg'
+    }
+
+  ],
+});
+
+</script>
